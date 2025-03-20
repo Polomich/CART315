d082f46ddfa038f8b127445b20b96467c86f49db
polomich <michpolonsky@gmail.com>
2025-03-20 16:13:38 -0400

    update colors

--- Games/pixeltime/index.js
+++ Games/pixeltime/index.js
@@ -1,48 +1,54 @@
 var gameTable;
 var gameTiles;
-var colors = ["none", "red", "orange", "yellow", "green"];
-
-console.log(colors.indexOf(""));
-console.log(colors.indexOf("none"));
-console.log(colors.indexOf("red"));
-console.log(colors.indexOf("orange"));
+var colors = ["white", "red", "orange", "yellow", "green"];
+var vertCoord;
+var horizCoord;
 
 function onLoadFunc() {
   gameTable = document.getElementById("game-table");
-  //   console.log(gameTable);
   gameTiles = gameTable.getElementsByTagName("td");
-  //   console.log(gameTiles);
-  //   console.log(typeof gameTiles);
 
   for (let tile of gameTiles) {
     tile.setAttribute("class", "tile");
     tile.setAttribute("onclick", "changeColor(this)");
   }
+
+  populateCoordinates();
+}
+
+function populateCoordinates() {
+  vertCoord = document.getElementById("vert-coord").getElementsByTagName("td");
+  console.log(vertCoord);
+
+  var i = 1;
+  for (let tile of vertCoord) {
+    tile.textContent = i;
+    i++;
+  }
+
+  horizCoord = document
+    .getElementById("horiz-coord")
+    .getElementsByTagName("td");
+  i = 65;
+  for (let tile of horizCoord) {
+    tile.textContent = String.fromCharCode(i);
+    i++;
+  }
 }
 
 function changeColor(tile) {
-  console.log(tile);
   let currColor = tile.style.background;
   var colorIndex = colors.indexOf(currColor);
-  console.log("currColor: " + currColor + " index: " + colorIndex);
 
   if (colorIndex == -1) {
-    console.log("kkkkk");
     colorIndex = 0;
   } else if (colorIndex == colors.length - 1) {
-    console.log("jjjjj");
     colorIndex = -1;
   }
 
   tile.style.background = colors[colorIndex + 1];
-  console.log(
-    "next color: " +
-      tile.style.background +
-      " index: " +
-      colors.indexOf(tile.style.background)
-  );
-  //   console.log(colorIndex + 1);
-  //   console.log(colors[colorIndex + 1]);
 }
 
-function colorToIndex() {}
+function setUpGame(){
+  
+}
